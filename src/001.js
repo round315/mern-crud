3f2ac6a3b90f67c25fb3f35313f476e7b25cdcf7
round315 <roundearthmoon@gmail.com>
2022-06-09 21:34:50 -0400

    update app co,mmit

--- src/001.js
+++
@@ -1,7 +0,0 @@
-async function fetchMethod() {
-    let response = await fetch(
-        "http://sampleapi.de/v1/find");
-    let data = await response.json();
-    console.log(data);
-}
-fetchMethod();
